7d6a59add94bc8f63ae52dd964beef6d658cb454
Kabirdas Henry <kabirdas@retool.com>
2022-11-30 17:36:43 -0800

    switch to retoolin app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Retool from "react-retool";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+  }
+
+  buttonClicked(event) {
+    this.setState({ value: this.state.value + 1 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="parent-react-app-container">
+          <h1>I am a react app</h1>
+          <h2>
+            Value Retool will read on load:{" "}
+            <span id="id-value-retool-reads">hello</span>
+          </h2>
+          <div>
+            <h2>
+              <span id="other-id-value-retool-reads">{this.state.value}</span>
+            </h2>
+            <button onClick={this.buttonClicked}>
+              Increment Dynamic Value
+            </button>
+          </div>
+        </div>
+
+        <div className="retool-box">
+          <Retool height="600px"  url="https://retoolin.tryretool.com/editor/52327434-7118-11ed-9ffe-671e4dff40d1/Bugs/parent%20window%20query%20not%20reading%20" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
